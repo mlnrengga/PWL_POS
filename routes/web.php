11450f305dc397f83632ccc84c4ed78e136ba391
Maulana Rengga Ramadan <mlnrengga@gmail.com>
2025-03-12 03:05:00 +0700

    Add: Tugas_m_barang_W5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\SupplierController;
@@ -51,4 +52,15 @@ Route::group(['prefix' => 'kategori'], function () {
     Route::get('/{id}/edit', [KategoriController::class, 'edit']);  
     Route::put("/{id}", [KategoriController::class, 'update']);       
     Route::delete('/{id}', [KategoriController::class, 'destroy']);  
+});
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);             
+    Route::post('/list', [BarangController::class, 'list']);       
+    Route::get('/create', [BarangController::class, 'create']);    
+    Route::post("/", [BarangController::class, 'store']);         
+    Route::get('/{id}', [BarangController::class, 'show']);        
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);  
+    Route::put("/{id}", [BarangController::class, 'update']);       
+    Route::delete('/{id}', [BarangController::class, 'destroy']);  
 });
