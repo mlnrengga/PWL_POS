b8e1ffa6bdd80c7472b6337bbe5f7f0bb83cc74a
Maulana Rengga Ramadan <mlnrengga@gmail.com>
2025-03-18 02:07:51 +0700

    Add: Praktikum3_W6

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::put("/{id}", [UserController::class, 'update']);       // menyimpan perubahan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user Ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user Ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 });
 
