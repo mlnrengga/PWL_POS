a135de3a409eb73afff551c10813e1122a17144f
Maulana Rengga Ramadan <mlnrengga@gmail.com>
2025-04-09 18:10:44 +0700

    Add: Praktikum1_W8

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -6,6 +6,7 @@ use App\Models\BarangModel;
 use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Yajra\DataTables\Facades\DataTables;
 
 class BarangController extends Controller
@@ -303,5 +304,66 @@ class BarangController extends Controller
                 $barang = BarangModel::find($id);
                 $kategori = KategoriModel::select('kategori_id', 'kategori_nama')->get();
                 return view('barang.show_ajax', ['barang' => $barang, 'kategori' => $kategori]);
+            }  
+
+            public function import()
+            {
+                return view('barang.import'); // file view: resources/views/barang/import.blade.php
             }
+
+            public function import_ajax(Request $request)
+            {
+                if($request->ajax() || $request->wantsJson()){
+                    $rules = [
+                        'file_barang' => ['required', 'mimes:xlsx', 'max:1024']
+                    ];
+            
+                    $validator = Validator::make($request->all(), $rules);
+                    if($validator->fails()){
+                        return response()->json([
+                            'status' => false,
+                            'message' => 'Validasi Gagal',
+                            'msgField' => $validator->errors()
+                        ]);
+                    }
+            
+                    $file = $request->file('file_barang');
+                    $reader = IOFactory::createReader('Xlsx');
+                    $reader->setReadDataOnly(true);
+                    $spreadsheet = $reader->load($file->getRealPath());
+                    $sheet = $spreadsheet->getActiveSheet();
+                    $data = $sheet->toArray(null, false, true, true);
+            
+                    $insert = [];
+                    if(count($data) > 1){
+                        foreach ($data as $baris => $value) {
+                            if($baris > 1){
+                                $insert[] = [
+                                    'kategori_id' => $value['A'],
+                                    'barang_kode' => $value['B'],
+                                    'barang_nama' => $value['C'],
+                                    'harga_beli' => $value['D'],
+                                    'harga_jual' => $value['E'],
+                                    'created_at' => now(),
+                                ];
+                            }
+                        }
+            
+                        if(count($insert) > 0){
+                            BarangModel::insertOrIgnore($insert);
+                        }
+            
+                        return response()->json([
+                            'status' => true,
+                            'message' => 'Data berhasil diimport'
+                        ]);
+                    } else {
+                        return response()->json([
+                            'status' => false,
+                            'message' => 'Tidak ada data yang diimport'
+                        ]);
+                    }
+                }
+                return redirect('/');
+            }            
 }
--- routes/web.php
+++ routes/web.php
@@ -140,6 +140,8 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
         Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']); // ajax update
         Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); // ajax form confirm
         Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // ajax delete
+        Route::get('/barang/import', [BarangController::class, 'import']); // ajax form upload excel
+        Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
     });
 
     Route::middleware(['authorize:ADM,MNG,STF'])->group(function () {
